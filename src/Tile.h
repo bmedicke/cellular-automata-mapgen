8e9bb0e906acfaa49007ff846a71967a73ca936f
Benjamin Medicke <bmedicke@gmail.com>
2011-07-24 18:43:05 +0200

    create a grid of evenly spaced tiles, added Tile.h

---
+++ src/Tile.h
@@ -0,0 +1,17 @@
+//
+//  Tile.h
+//  mapgen
+//
+//  Created by Benjamin Medicke on 24.07.11.
+//  Copyright 2011 Benjamin Medicke. All rights reserved.
+//
+
+#ifndef mapgen_Tile_h
+#define mapgen_Tile_h
+
+struct Tile
+{
+    float x, y;
+};
+
+#endif
--- src/mapgenApp.cpp
+++ src/mapgenApp.cpp
@@ -1,5 +1,10 @@
+// include local files first, to make sure they include everything they need:
+#include "Tile.h"
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+
+#include <algorithm>
 
 using namespace ci;
 using namespace ci::app;
@@ -12,19 +17,41 @@ public:
 	void mouseDown( MouseEvent event );	
     void keyDown( KeyEvent event );
     void keyUp( KeyEvent event );
+    void resize( ResizeEvent event );
 	void update();
 	void draw();
-private:
-    Vec2f center;
+
+private:    
     bool wireframe;
+    
+    static const int mapSize = 30;
+    Tile tiles[mapSize][mapSize];
 };
 
 void mapgenApp::setup()
 {
-    setWindowSize( 500, 500 );
+    setWindowSize( 800, 800 );
     setFrameRate( 60.0f );
     setFullScreen( false );
+    
     wireframe = false;
+
+    // GRIDCODE:
+    
+    // space the tiles along the smaller dimension to make sure
+    // that they are all visible:
+    int spacing = std::min( getWindowWidth(), getWindowHeight() ) / mapSize;
+    
+    for ( int x = 0 ; x < mapSize ; x++ )
+    {
+        for (int y = 0 ; y < mapSize ; y++ )
+        {
+            // space the tiles evenly with an offset of
+            // 0.5 spacing to center the grid:
+            tiles[x][y].x = x * spacing + 0.5 * spacing;
+            tiles[x][y].y = y * spacing + 0.5 * spacing;
+        }
+    }
 }
 
 void mapgenApp::mouseDown( MouseEvent event )
@@ -37,6 +64,10 @@ void mapgenApp::keyDown( KeyEvent event )
             
         case KeyEvent::KEY_w:
             wireframe = !wireframe;
+            if ( wireframe )
+                enableWireframe();
+            else
+                disableWireframe();
             break;
             
         case KeyEvent::KEY_f:
@@ -55,21 +86,33 @@ void mapgenApp::keyUp( KeyEvent event )
 {
 }
 
+void mapgenApp::resize( ResizeEvent event )
+{
+    // realign the tiles.
+}
 
 void mapgenApp::update()
 {
-    if ( wireframe )
-        enableWireframe();
-    else
-        disableWireframe();
-    
-    center.set( getWindowWidth() * 0.5, getWindowHeight() * 0.5 );
 }
 
 void mapgenApp::draw()
 {
 	clear();
-    drawCube( Vec3f( center, 10.0 ), Vec3f( 100.0, 100.0, 100.0 ) );
+    
+    
+    // GRIDCODE:
+    // make the size slightly smaller than the spacing 
+    // to distinguish them from each other:
+    int size = std::min( getWindowWidth(), getWindowHeight() ) / mapSize * 0.9;
+    
+    for ( int x = 0 ; x < mapSize ; x++ )
+    {
+        for (int y = 0 ; y < mapSize ; y++ )
+        {
+            drawCube(   Vec3f( tiles[x][y].x , tiles[x][y].y, 0 ), 
+                        Vec3f( size, size, size ) );
+        }
+    }
 }
 
 
